cd9d2618928809bab90311f37cc1726663b4ad13
Garrett <garres4@vt.edu>
2021-10-09 15:23:35 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Jacoby Burns';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
